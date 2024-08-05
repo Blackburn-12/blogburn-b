506be28172a17e723526035f5ee239048dc2cb8f
Muhammad Abdullah <iamagoodboi4@gmail.com>
2024-08-05 19:19:25 +0500

    idk

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from "./config/db.js"
-import authRoutes from './routes/authRoutes.js';
-import postRoutes from './routes/postRoutes.js';
-import commentRoutes from './routes/commentRoutes.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import uploadRoutes from './routes/uploadRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 // Load environment variables
 dotenv.config();
-
 
 // Connect to MongoDB
 connectDB();
@@ -19,32 +18,33 @@ connectDB();
 const app = express();
 
 // Middleware
-const allowedOrigins = [
-    'https://blogburn.vercel.app'
-  ];
-  
-  // Configure CORS middleware
-  app.use(cors({
+const allowedOrigins = ["https://blogburn.vercel.app", "http://localhost:5173"];
+
+// Configure CORS middleware
+app.use(
+  cors({
     origin: function (origin, callback) {
       // Allow requests with no origin (like mobile apps, curl requests)
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
         return callback(new Error(msg), false);
       }
       return callback(null, true);
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
     credentials: true,
-  }));
+  })
+);
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/comments', commentRoutes);
-app.use('/api/upload', uploadRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/upload", uploadRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
